0d6a559081159fbfe4b7cf8ed28d54a741a69f6c
mPAND <manueldavidpandian@gmail.com>
2014-07-19 12:12:24 +0530

    removed old comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#makeCacheMatrix <- function(x = matrix()) {
+#cacheSolve <- function(x, ...) {
 
 #
 # This is a two step process
